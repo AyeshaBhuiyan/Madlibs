d0f80cb3e608b9e6497cb3181244e03f5d9ed96c
Ayesha Bhuiyan <abhuiyan122@gmail.com>
2024-08-02 20:15:24 +0000

    madlibs

---
+++ script.js
@@ -0,0 +1,12 @@
+//Sentences: I knew what she was saying wasn't true (t/f). There's no way today is the 31 (#) of August (input). There's no way I was asleep (word) for 3 (#value) months. 
+
+word = "asleep"
+let boolean = true
+number = 31
+calcnum = 2 + 1
+let input = prompt("Enter a Month: ")
+console.log("I knew what she was saying wasn't " + boolean + ". There was no way today was the " + number + "st of " + input + ". There was no way I was " + word + 
+    " for " + calcnum + " months."
+)
+
+
